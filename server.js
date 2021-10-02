a47b79577819d0770e9dc1fcc5fe508fe9f0b66a
Upendra12395 <ch95upendra@gmail.com>
2021-10-02 22:21:28 +0530

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT
+
+
+app.get('/', (req, res)=>{
+    res.send("hello")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`app running on ${PORT}`)
+})
+
