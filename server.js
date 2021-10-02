16281af391e81e95a3d13375ed1dc5ff347354a6
Upendra12395 <ch95upendra@gmail.com>
2021-10-02 22:31:24 +0530

    sequelize config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require("cors");
 
 const app = express()
 const PORT = process.env.PORT
@@ -9,6 +10,8 @@ app.get('/', (req, res)=>{
     res.send("hello")
 })
 
+const db = require("./app/models");
+db.sequelize.sync();
 
 app.listen(PORT, ()=>{
     console.log(`app running on ${PORT}`)
